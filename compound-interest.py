269160c98a24f03b0b073174585254b986754f8c
sc-ai <56286725+sc-ai@users.noreply.github.com>
2019-10-08 19:08:18 +0530

    compound-interest.py
    the formula of compound interest is:  P(1+R/100)*r   
    
    where, 
    
    p = principal amount
    
    R= rate of interest
    
    T= time span

---
+++ compound-interest.py
@@ -0,0 +1,11 @@
+n1=int(input("Enter the principle amount:"))
+
+rate=int(input("Enter the rate:"))
+
+years=int(input("Enter the number of years:"))
+
+for i in range(years):
+
+    n1=n1+((n1*rate)/100)
+
+print(n)
